3347c9d4009d4a95b478dd7d405ea365bb8ef197
Agnik Sarkar <121817949+agniksarkar1107@users.noreply.github.com>
2024-02-29 19:19:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st 
+from PIL import Image
 
+im = Image.open('')
+st.set_page_config( page_title="IemCrp Result", page_icon = im ,layout="wide")  
+
+
+new_image = im.resize((150, 100))
+   
+
+
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)  
+  
